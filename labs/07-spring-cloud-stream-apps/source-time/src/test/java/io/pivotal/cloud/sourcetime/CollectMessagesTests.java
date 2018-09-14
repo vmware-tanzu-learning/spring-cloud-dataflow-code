38b0cdacc8f143b052760c7c18e6efc832bcaaa6
Eitan Suez <esuez@pivotal.io>
2018-09-14 15:21:31 -0500

    updated gradle wrapper version; added tests from testing lab for source-time app

---
+++ labs/07-spring-cloud-stream-apps/source-time/src/test/java/io/pivotal/cloud/sourcetime/CollectMessagesTests.java
@@ -0,0 +1,30 @@
+package io.pivotal.cloud.sourcetime;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CollectMessagesTests {
+
+	@Autowired
+	private Source timeSource;
+
+	@Autowired
+	private MessageCollector messageCollector;
+
+
+	@Test
+	public void sourceTimeTest() throws InterruptedException {
+		Thread.sleep(5000);
+		assertThat(messageCollector.forChannel(timeSource.output()).size()).isGreaterThan(3);
+	}
+
+}
---
+++ labs/07-spring-cloud-stream-apps/source-time/src/test/java/io/pivotal/cloud/sourcetime/GetMessageTests.java
@@ -0,0 +1,36 @@
+package io.pivotal.cloud.sourcetime;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.Message;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GetMessageTests {
+
+	private final Logger log = LoggerFactory.getLogger(GetMessageTests.class);
+
+	@Autowired
+	private MessageSource<String> sourceTime;
+
+	@Test
+	public void contextLoads() {
+		assertNotNull(sourceTime);
+	}
+
+	@Test
+	public void simpleSourceTimeTest() {
+		Message<String> message = sourceTime.receive();
+		assertNotNull(message);
+		log.info(message.getPayload());
+	}
+
+}
