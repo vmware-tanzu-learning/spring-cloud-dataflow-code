4103c5066ae48c2e48a03d4b48792714cd46b9e3
sashinpivotal <sashin@pivotal.io>
2019-09-21 14:57:58 +0100

    change spring boot 2.0.3 to 2.0.6

--- labs/30-batch/batch-solution/src/main/java/io/pivotal/education/batch/BatchApplication.java
+++ labs/30-batch/batch-solution/src/main/java/io/pivotal/education/batch/BatchApplication.java
@@ -6,11 +6,13 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 @EnableBatchProcessing
+@EnableTask
 @EntityScan("io.pivotal.education.domain")
 public class BatchApplication {
 
